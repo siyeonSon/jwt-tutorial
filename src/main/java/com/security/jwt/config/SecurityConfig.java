dc86bc5ad97f967e215f27dae8d01d9106f1bfad
siyeonSon <siyeon0209@naver.com>
2023-04-10 22:57:26 +0900

    chore(jwt): SecurityConfig에 JWT 관련 설정 추가

--- src/main/java/com/security/jwt/config/SecurityConfig.java
+++ src/main/java/com/security/jwt/config/SecurityConfig.java
@@ -1,13 +1,31 @@
 package com.security.jwt.config;
 
+import com.security.jwt.jwt.JwtAccessDeniedHandler;
+import com.security.jwt.jwt.JwtAuthenticationEntryPoint;
+import com.security.jwt.jwt.JwtSecurityConfig;
+import com.security.jwt.jwt.TokenProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final TokenProvider tokenProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     // h2 database 테스트가 원활하도록 관련 API 들은 전부 무시
     @Bean
@@ -18,15 +36,32 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
+        // CSRF 설정 Disable
+        http.csrf().disable()
+
+                // exception handling 할 때 우리가 만든 클래스를 추가
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler)
+                .and()
+
+                .headers(headers -> headers.frameOptions().sameOrigin())
+
+                // security 는 기본적으로 세션을 사용
+                // 여기서는 세션을 사용하지 않기 때문에 세션 설정을 stateless 로 설정
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+
+                // 로그인, 회원가입 API는 토큰이 없는 상태에서 요청이 들어오기 때문에 permitAll 설정
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/**").permitAll()
-                        .anyRequest().authenticated()
+                        .requestMatchers("/auth/**").permitAll()
+                        .anyRequest().authenticated()   // 나머지 API 는 모두 인증 필요
                 )
-                .headers(headers -> headers.frameOptions().disable())
-                .csrf(csrf -> csrf
-                        .ignoringRequestMatchers("/h2-console/**")
-                        .disable());
+
+                // JwtFilter 를 addFilterBefore 로 등록했던 JwtSecurityConfig 클래스를 적용
+                .apply(new JwtSecurityConfig(tokenProvider));
+
         return http.build();
     }
 }
