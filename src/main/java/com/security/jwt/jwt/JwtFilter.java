1a4f67fda6ac76b2e6f7a475f5a4eaa54f3e857f
siyeonSon <siyeon0209@naver.com>
2023-04-10 22:33:21 +0900

    chore(jwt): 토큰의 인증 정보를 Security Context에 저장하기 위한 JwtFilter 생성

---
+++ src/main/java/com/security/jwt/jwt/JwtFilter.java
@@ -0,0 +1,54 @@
+package com.security.jwt.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.GenericFilterBean;
+
+import java.io.IOException;
+
+public class JwtFilter extends GenericFilterBean {
+
+    private static final Logger logger = LoggerFactory.getLogger(JwtFilter.class);
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+    private TokenProvider tokenProvider;
+
+    public JwtFilter(TokenProvider tokenProvider) {
+        this.tokenProvider = tokenProvider;
+    }
+
+    // doFilter 역할: 토큰의 인증 정보를 Security Context에 저장
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest)request;
+        String jwt = resolveToken(httpServletRequest);
+        String requestURI = httpServletRequest.getRequestURI();
+
+        if (StringUtils.hasText(jwt) && tokenProvider.validateToken(jwt)) {
+            Authentication authentication = tokenProvider.getAuthentication(jwt);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            logger.debug("Security Context 에 '{}' 인증 정보를 저장했습니다, uri: {}", authentication.getName(), requestURI);
+        }
+        else {
+            logger.debug("유효한 JWT 토큰이 없습니다, uri: {}", requestURI);
+        }
+        chain.doFilter(request, response);
+    }
+
+    // resolveToken 역할: request에서 토큰 정보를 가져옴
+    private String resolveToken(HttpServletRequest request) {
+        String bearToken = request.getHeader(AUTHORIZATION_HEADER);
+
+        if (StringUtils.hasText(bearToken) && bearToken.startsWith("Bearer ")) {
+            return bearToken.substring(7);
+        }
+        return null;
+    }
+}
