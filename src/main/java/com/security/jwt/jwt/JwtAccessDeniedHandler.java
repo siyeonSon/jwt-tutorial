cfc57bb54d988d99b99a86e11b27bc44a45ecbf9
siyeonSon <siyeon0209@naver.com>
2023-04-10 22:42:56 +0900

    chore(jwt): 필요한 권한이 없어 접근하려 할 때 403

---
+++ src/main/java/com/security/jwt/jwt/JwtAccessDeniedHandler.java
@@ -0,0 +1,18 @@
+package com.security.jwt.jwt;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import java.io.IOException;
+
+public class JwtAccessDeniedHandler implements AccessDeniedHandler
+{
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        // 필요한 권한이 없어 접근하려 할 때 403
+        response.sendError(HttpServletResponse.SC_FORBIDDEN);
+    }
+}
